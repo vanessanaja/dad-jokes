5421c5533c9834e0820348af508c6847cea99866
vanessanaja <vanessacnaja@gmail.com>
2019-06-21 09:53:54 -0700

    add state to jokesList

--- src/JokeList.js
+++ src/JokeList.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class JokeList extends Component {
-  // constructor(props){
-  //   super(props);
-  // }
+  static defaultProps = {
+    numJokesToGet: 10
+  }
+  constructor(props){
+    super(props);
+    this.state = {jokes: []}
+  }
   async componentDidMount(){
-    let res = await axios.get('https://icanhazdadjoke.com/');
-    console.log(res);
+    let res = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
+    console.log(res.data.joke);
   }
   render(){
     return (
