d697cc8ed34659918ab8b035ff3a0140f9da05b9
vanessanaja <vanessacnaja@gmail.com>
2019-06-21 10:16:12 -0700

    render jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -7,16 +7,27 @@ class JokeList extends Component {
   }
   constructor(props){
     super(props);
-    this.state = {jokes: []}
+    this.state = { jokes: [] };
   }
   async componentDidMount(){
-    let res = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
-    console.log(res.data.joke);
+    let jokes = [];
+    while(jokes.length < this.props.numJokesToGet){
+      let res = await axios.get('https://icanhazdadjoke.com/',
+      {headers: {Accept: 'application/json'}
+    });
+    jokes.push(res.data.joke);
+    }
+    this.setState({ jokes: jokes })
   }
   render(){
     return (
-      <div>
+      <div className='JokeList'>
         <h1>Dad Jokes</h1>
+        <div className='JokeList-jokes'>
+         {this.state.jokes.map(j => (
+           <div>{j}</div>
+         ))}
+        </div>
       </div>
     )
   }
