d7b6964e7b53511bc5be012e9990a896d3e20422
vanessanaja <vanessacnaja@gmail.com>
2019-06-22 10:56:42 -0700

    style jokes

--- src/Joke.js
+++ src/Joke.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Joke.css';
 
 class Joke extends Component {
   render(){
@@ -6,7 +7,7 @@ class Joke extends Component {
       <div className='Joke'>
         <div className='Joke-buttons'>
           <i className='fas fa-arrow-up' onClick={this.props.upvote}></i>
-          <span>{this.props.votes}</span>
+          <span className='Jokes-vote'>{this.props.votes}</span>
           <i className='fas fa-arrow-down' onClick={this.props.downvote}></i>
         </div>
         <div className='Joke-text'>
